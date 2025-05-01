7eef1f5cac72704b8aa1f8ed5b65543a8c7ab745
AllisonVelasquez <allison.velasquezy.ja@gmail.com>
2025-05-01 16:36:53 +0200

    directorio movies arreglado

--- mediaclub_laravel/app/Http/Controllers/MovieController.php
+++ mediaclub_laravel/app/Http/Controllers/MovieController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Services\TmdbService;
+use App\Integrations\Tmdb\TmdbService;
 use Exception;
 use App\Traits\ApiResponse;
 
--- mediaclub_laravel/app/Integrations/Tmdb/TmdbService.php
+++ mediaclub_laravel/app/Integrations/Tmdb/TmdbService.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Services;
+namespace App\Integrations\Tmdb;
 
 use Illuminate\Support\Facades\Http;
 
