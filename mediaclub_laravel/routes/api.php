d42b37db264a019ea60d76d37bf0f2d467e0d5b8
AllisonVelasquez <allison.velasquezy.ja@gmail.com>
2025-05-01 16:13:01 +0200

    api.php actualizado users, inicio tmdb

--- mediaclub_laravel/routes/api.php
+++ mediaclub_laravel/routes/api.php
@@ -5,18 +5,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
 
 //API USERS
-Route::get('/users', [userController::class, 'index']);
-
-Route::get('users/{id}', [userController::class,'show']);
-
-Route::post('/users', [userController::class,'store']);
-
-Route::put('users/{id}', [userController::class,'update']);
-
-Route::patch('users/{id}', [userController::class,'updatePartial']);
-
-Route::delete('users/{id}', [userController::class,'delete']);
-
+Route::prefix('users')->group(function () {
+    Route::get('/', [userController::class, 'index']);
+    Route::get('/{id}', [userController::class, 'show']);
+    Route::post('/', [userController::class, 'store']);
+    Route::put('/{id}', [userController::class, 'update']);
+    Route::patch('/{id}', [userController::class, 'updatePartial']);
+    Route::delete('/{id}', [userController::class, 'delete']);
+});
 
 //API TMDB
-Route::get('/movies/popular',[MovieController::class,'popular']);
+Route::prefix('movies')->group(function () {
+    // Route::get('/', [MovieController::class, 'index']);
+    Route::get('/popular', [MovieController::class, 'popular']);
+});
