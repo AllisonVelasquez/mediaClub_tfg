e0a408a31bd9874979ec0121bf8cfcea52fbba86
AllisonVelasquez <allison.velasquezy.ja@gmail.com>
2025-05-08 22:21:16 +0200

    sanctum instalado

--- mediaclub_laravel/routes/api.php
+++ mediaclub_laravel/routes/api.php
@@ -6,16 +6,28 @@ use App\Http\Controllers\userController;
 
 //API USERS
 Route::prefix('usuarios')->group(function () {
+    //Gets
+
     Route::get('/', [userController::class, 'index']);
     Route::get('/{id}', [userController::class, 'show']);
+
+    // Route::middleware('auth:sanctum')->get('/', [userController::class, 'index']);
+
+    //Post
     Route::post('/', [userController::class, 'store']);
+
+    //Put
     Route::put('/{id}', [userController::class, 'update']);
+
+    //Patch
     Route::patch('/{id}', [userController::class, 'updatePartial']);
+
+    //Delete
     Route::delete('/{id}', [userController::class, 'delete']);
 });
 
 //API TMDB
 Route::prefix('movies')->group(function () {
-    // Route::get('/', [MovieController::class, 'index']);
+
     Route::get('/popular', [TmdbController::class, 'popular']);
 });
