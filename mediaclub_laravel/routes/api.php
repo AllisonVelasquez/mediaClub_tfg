c8c20ad85c0404f372884979806be675e171e1fd
AllisonVelasquez <allison.velasquezy.ja@gmail.com>
2025-05-16 15:27:57 +0200

    rutas actualizacion nombres mas comprensibles

--- mediaclub_laravel/routes/api.php
+++ mediaclub_laravel/routes/api.php
@@ -20,62 +20,61 @@ Route::prefix('auth')->group(function () {
 
 Route::prefix('usuarios')->group(function () {
     Route::get('/alias/{alias}', [UserController::class, 'getIdByAlias']);
-    Route::get('/{id}', [UserController::class, 'showProfile']);
+    Route::get('/{id}/perfil', [UserController::class, 'showProfile']);
     Route::get('/{id}/listas-publicas', [UserController::class, 'showLists']);
     Route::get('/{id}/amigos', [UserController::class, 'showFriends']);
     Route::get('/{id}/actividad', [UserController::class, 'activity']);
 });
 
 Route::middleware('auth:sanctum')->prefix('me')->group(function () {
-    Route::get('/', [UserController::class, 'myProfile']);
+    Route::get('/perfil', [UserController::class, 'myProfile']);
     Route::put('/actualizar-datos', [UserController::class, 'updateProfile']);
     Route::delete('/borrar-cuenta', [UserController::class, 'deleteUser']);
     Route::get('/amigos', [UserController::class, 'myFriends']);
+    Route::delete('/amigos/eliminar/{id}', [UserController::class, 'removeFriend']);
     Route::get('/actividad', [UserController::class, 'myActivity']);
 
     // Agrupar bajo autenticación
     Route::prefix('amistad')->group(function () {
         Route::post('/solicitar/{id}', [FriendshipController::class, 'sendRequest']);
         Route::delete('/cancelar-solicitud/{id}', [FriendshipController::class, 'cancelRequest']);
-        Route::post('/aceptar/{id}', [FriendshipController::class, 'acceptRequest']);
-        Route::post('/rechazar/{id}', [FriendshipController::class, 'rejectRequest']);
-        Route::get('/solicitudes', [FriendshipController::class, 'viewRequests']);
-        Route::get('/enviadas', [FriendshipController::class, 'viewSentRequests']);
-        Route::delete('/amigos/{id}', [UserController::class, 'removeFriend']);
+        Route::post('/aceptar-solicitud/{id}', [FriendshipController::class, 'acceptRequest']);
+        Route::post('/rechazar-solicitud/{id}', [FriendshipController::class, 'rejectRequest']);
+        Route::get('/solicitudes-recibidas', [FriendshipController::class, 'viewRequests']);
+        Route::get('/solicitudes-enviadas', [FriendshipController::class, 'viewSentRequests']);
     });
 
 
     Route::prefix('listas')->group(function () {
-        Route::get('/', [ListController::class, 'myLists']);
-        Route::get('/{id}', [ListController::class, 'showList']);
-        Route::post('/', [ListController::class, 'addList']);
-        Route::put('/{id}', [ListController::class, 'editList']);
-        Route::delete('/{id}', [ListController::class, 'deleteList']);
-        Route::post('/{id}/agregar-frame', [ListController::class, 'addContent']);
-        Route::delete('/{id}/quitar/{contenidoId}', [ListController::class, 'removeContent']);
+        Route::get('/ver-todas', [ListController::class, 'myLists']);
+        Route::post('/crear', [ListController::class, 'addList']);
+        Route::put('/editar/{id}', [ListController::class, 'editList']);
+        Route::delete('/borrar/{id}', [ListController::class, 'deleteList']);
+        Route::get('/{id}/detalles', [ListController::class, 'showList']);
+        Route::post('/{id}/anadir/{frameid}', [ListController::class, 'addContent']);
+        Route::delete('/{id}/quitar/{frameid}', [ListController::class, 'removeContent']);
     });
 
     Route::prefix('resenas')->group(function () {
-        Route::get('/', [ResenaController::class, 'getReviews']);
-        Route::post('/', [ResenaController::class, 'addReview']);
-        Route::get('/{id}', [ResenaController::class, 'showReview']);
-        Route::delete('/{id}', [ResenaController::class, 'deleteReview']);
-        Route::get('/{titulo}/{id}', [ResenaController::class, 'showFrameReview']);
+        Route::get('/ver-todas', [ResenaController::class, 'getReviews']);
+        Route::post('/crear', [ResenaController::class, 'addReview']);
+        Route::delete('/borrar/{id}', [ResenaController::class, 'deleteReview']);
+        Route::get('/titulo/{Frameid}', [ResenaController::class, 'showFrameReview']);
     });
 
     Route::prefix('puntuaciones')->group(function () {
-        Route::get('/', [PuntuacionController::class, 'getRating']);
-        Route::post('/', [PuntuacionController::class, 'addRating']);
-        Route::put('/{id}', [PuntuacionController::class, 'editRating']);
-        Route::delete('/{id}', [PuntuacionController::class, 'deleteRating']);
-        Route::get('/{titulo}/{id}', [PuntuacionController::class, 'showFrameRating']);
+        Route::get('/ver-todas', [PuntuacionController::class, 'getRating']);
+        Route::post('/crear', [PuntuacionController::class, 'addRating']);
+        Route::put('/editar/{id}', [PuntuacionController::class, 'editRating']);
+        Route::delete('/borrar/{id}', [PuntuacionController::class, 'deleteRating']);
+        Route::get('/titulo/{Frameid}', [PuntuacionController::class, 'showFrameRating']);
     });
 });
 
 
 // Contenido cambiar controllers
 Route::prefix('frames')->group(function () {
-    Route::get('/buscar/{titulo}', [FrameController::class, 'search']);
+    Route::get('/buscar/{titulo}', [FrameController::class, 'searchByTitle']);
     Route::get('/peliculas', [TmdbController::class, 'getMovies']);
     Route::get('/series', [TmdbController::class, 'getSeries']);
     Route::get('/popular', [TmdbController::class, 'popular']);
@@ -84,7 +83,6 @@ Route::prefix('frames')->group(function () {
     Route::get('/tendencia', [TmdbController::class, 'trending']);
     Route::get('/{id}', [FrameController::class, 'show']);
     Route::get('/{id}/similar', [TmdbController::class, 'similarMovies']); //Va a mostrar todo TODO
-     
     // Route::post('/sincronizar/{id}', [FrameController::class, 'sincronizar']);
 });
 
