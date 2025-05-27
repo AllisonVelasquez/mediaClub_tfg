08a563f1bc43d59ff6b10a1be7af7042f2dcda03
AllisonVelasquez <allison.velasquezy.ja@gmail.com>
2025-05-27 18:12:12 +0200

    user con token

--- mediaclub_laravel/app/Models/Usuario.php
+++ mediaclub_laravel/app/Models/Usuario.php
@@ -8,7 +8,9 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 /**
  * Class Usuario
@@ -37,8 +39,10 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @package App\Models
  */
-class Usuario extends Model
+class Usuario extends Authenticatable 
 {
+	use HasApiTokens;
+	
 	protected $table = 'usuario';
 	protected $primaryKey = 'usuario_id';
 	public $timestamps = false;
