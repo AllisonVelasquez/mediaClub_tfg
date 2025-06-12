997f8469adcf98c9b08f06033031ca5d92e37fb4
AllisonVelasquez <allison.velasquezy.ja@gmail.com>
2025-06-12 21:38:39 +0200

    cambiada una variable :)

--- mediaclub_react/src/services/Frames/CRUD_Frames.js
+++ mediaclub_react/src/services/Frames/CRUD_Frames.js
@@ -37,8 +37,8 @@ export const getFramesPopulares = async () => {
     // Obtener frames por género con paginación
 export const getFramesByGenero = async (genero, pagina = 1) => {
   try {
-    const response = await instance.get("frames/filtrar/?gerno=", {
-      params: { genero:genero, page: pagina },
+    const response = await instance.get("frames/filtrar", {
+      params: { genero: genero, page: pagina },
     });
     console.log("Respuesta de getFramesByGenero:", response.data.contenido);
     
